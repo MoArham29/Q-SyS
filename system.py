8c9c1081b0544efe9bd4eeed55c50f7b0bc26943
MoArham29 <22428286@bucks.ac.uk>
2025-12-08 13:38:06 +0000

    i updated the system file to use the json storage

--- system.py
+++ system.py
@@ -1,23 +1,34 @@
 
 from customer import Customer
 from QStructure import Queue
+from storage import load_data, save_data
+
 
 class QueueSystem:
     def __init__(self):
+        customers_list, customers_dict = load_data()
+
         self.queue = Queue()
-        self.customers_by_ticket = {}
+        for c in customers_list:
+            self.queue.enqueue(c)
+
+        self.customers_by_ticket = customers_dict
+
 
     def register_customer(self, name, reason):
-        """Create and enqueue a new customer."""
         customer = Customer(name, reason)
         self.queue.enqueue(customer)
         self.customers_by_ticket[customer.ticket] = customer
+
+        save_data(self.list_waiting(), self.customers_by_ticket)
         return customer
 
+
     def call_next(self):
-        """Dequeue next customer."""
         next_customer = self.queue.dequeue()
+        save_data(self.list_waiting(), self.customers_by_ticket)
         return next_customer
+
 
     def search_by_ticket(self, ticket_number):
         """Lookup customer by ticket."""
