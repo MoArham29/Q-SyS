f2371c6b4d7b9d150df1cdd00ac1310561742117
MoArham29 <22428286@bucks.ac.uk>
2025-12-08 23:13:47 +0000

    added enter email and phone number

--- customer.py
+++ customer.py
@@ -3,13 +3,16 @@ import datetime
 class Customer:
     NextTicket = 1
 
-    def __init__(self, name: str, reason: str):
+    def __init__(self, name, reason, phone, email):
         self.ticket = Customer.NextTicket
         Customer.NextTicket += 1
 
         self.name = name
         self.reason = reason
+        self.phone = phone
+        self.email = email
         self.arrival = datetime.datetime.now()
 
     def __repr__(self):
-        return f"Customer(ticket={self.ticket}, name='{self.name}', reason='{self.reason}')"
+        return (f"Customer(ticket={self.ticket}, name='{self.name}', "
+                f"phone='{self.phone}', email='{self.email}')")
